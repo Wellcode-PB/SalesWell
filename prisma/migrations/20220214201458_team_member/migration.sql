a93473253747e766ec442405c6f1a60aa6442780
Petru Trimbitas <petru.trimbitas@yahoo.com>
2022-02-14 22:15:36 +0200

    Team member schema

---
+++ prisma/migrations/20220214201458_team_member/migration.sql
@@ -0,0 +1,14 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "team_members" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "mail" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "last_login" TIMESTAMP(3),
+    "role" "Role" NOT NULL DEFAULT E'USER',
+
+    CONSTRAINT "team_members_pkey" PRIMARY KEY ("id")
+);
