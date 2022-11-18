98db18ab15065e165a6eb68a7b10cf2505deeee1
Daniel Tanase <tanasedaniel54@gmail.com>
2022-11-18 16:02:04 +0200

    Search bar for prospects

---
+++ components/prospects/SearchProspect.tsx
@@ -0,0 +1,45 @@
+import CreateProspect from './CreateProspect'
+import { Alert, Divider, IconButton, InputBase, Paper } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search'
+import { useState } from 'react'
+
+function SearchProspect({ setProspectsList }) {
+  const [hasResults, setHasResults] = useState(null)
+
+  function searchProspect(event) {
+    const searchInput = event.target.value
+
+    fetch('/api/prospect/search?input=' + searchInput, {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then((response) => {
+      // first, empty the list
+      setProspectsList(() => [])
+
+      if (response.error) {
+        setHasResults({ message: response.error })
+      } else {
+        setHasResults(null)
+        setProspectsList((data) => [...data, ...response])
+      }
+    })
+  }
+
+  return (
+    <>
+      <Paper sx={{ display: 'flex', margin: '2% 10% 2% 10%' }} >
+        <IconButton><SearchIcon /></IconButton>
+        <InputBase id="search-bar" sx={{ flex: 1 }}
+            placeholder="Search Prospect" onChange={searchProspect} />
+        <Divider sx={{ height: 'auto', m: 0.5 }} orientation="vertical" />
+        <CreateProspect />
+      </Paper>
+      { hasResults ?
+        <Alert severity={'error'}>{hasResults.message}</Alert>
+        : null }
+    </>
+  )
+}
+
+export default SearchProspect
