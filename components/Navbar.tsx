d76cae337e976ee0a680205e36e50cbcd533eaa2
BunducPaul <117160428+BunducPaul@users.noreply.github.com>
2023-04-19 13:11:08 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,9 +11,11 @@ function NavBar() {
 
   return <AppBar position="static">
     <Toolbar>
-      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-        SalesWell
-      </Typography>
+     <Link href="/" passHref >
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} style={{ cursor: 'pointer' }}>
+          SalesWell
+        </Typography>
+      </Link>
       { session ? 
         <>
           <Link href="/booking/list">
