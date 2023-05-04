8c7943b50c0d9b6c5abd6c831b7ebec2086ced50
GabrielMaga1 <109106108+GabrielMaga1@users.noreply.github.com>
2023-05-04 13:33:41 +0300

    add the getServerSideProps function and delete the getBookingStatusTypes (#35)

--- pages/booking/list.tsx
+++ pages/booking/list.tsx
@@ -1,15 +1,24 @@
 import BookingCard from '../../components/booking/BookingCard'
 import BookingsFilter from '../../components/booking/BookingsFilter'
 import InfiniteScroll from 'react-infinite-scroll-component'
-import getBookingStatusTypes from '../../lib/get-booking-status-types'
 import getMorePageData from '../../lib/get-more-page-data'
+import prisma from '../../lib/prisma'
 
 import { useEffect, useState } from 'react'
 
-function BookingList() {
+export async function getServerSideProps() {
+  const statusTypes = await prisma.booking_status.findMany()
+  return { 
+    props: { 
+      bookingStatusTypes: JSON.parse(JSON.stringify(statusTypes)) 
+    } 
+  };
+}
+
+function BookingList({ bookingStatusTypes }) {
   const [bookings, setBookings] = useState([])
   const [hasMore, setHasMore] = useState(true)
-  const [statusesTypes, setStatusTypes] = useState()
+  const [statusesTypes, setStatusesTypes] = useState(bookingStatusTypes)
   const [orderBy, setOrderBy] = useState('default')
   const [sortOrder, setSortOrder] = useState('default')
 
@@ -23,10 +32,10 @@ function BookingList() {
     setSortOrder: setSortOrder,
     sortOrder: sortOrder
   }
-
+  
   useEffect(() => {
     getMorePageData(getMorePageDataParams)
-    getBookingStatusTypes(setStatusTypes)
+    setStatusesTypes(statusesTypes)
   }, [orderBy, sortOrder])
 
   return (
