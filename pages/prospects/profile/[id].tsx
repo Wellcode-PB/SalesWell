4426176c5a8c6bdfbccb1a2202572f8ce1708318
Ciprian Dumitriu <dumitriuciprian@gmail.com>
2023-05-10 17:02:10 +0300

    Fixed prospects user page now displays right data

--- pages/prospects/profile/[id].tsx
+++ pages/prospects/profile/[id].tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
-import { Divider, Grid, Paper, Typography } from '@mui/material'
+import { Box, Container, Divider, Grid, Grow, Paper, Typography } from '@mui/material'
 import ProfileInfo from '../../../components/prospects/ProfileInfo';
+import CustomError from '../../../components/CustomError';
 
 // use this function to get the 'id' parameter from the url
 export async function getServerSideProps({ params }) {
@@ -11,6 +12,8 @@ function ProspectProfile(props) {
   const [prospect, setProspect] = useState({
     name: "--", mail: "--", phone: "--", fb: "--"
   })
+  const [hasError, setHasError] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => { 
     fetch('/api/prospect/get-by-key?key=id&value=' + props.id, {
@@ -20,27 +23,48 @@ function ProspectProfile(props) {
     .then((response) => {
       // the response will be received as an array,
       // but we only need the first object
-      setProspect(response[0])
+      if (!response[0]) {
+        setHasError(true)
+        setIsLoading(false)
+      } else {
+        setProspect(response[0])
+        setIsLoading(false)
+      }
     })
   }, [])
 
-
-  return (
-    <Paper sx={{ p: 1, margin: '2%' }}>
-      <Grid container spacing={2}>
-        <Grid item xs={4}>
-          <ProfileInfo prospect={prospect}/>
+  if (hasError && !isLoading) {
+    const title = "Error 404"
+    const message = "The user you are searching for does not exist."
+    return (
+      <>
+        <CustomError title={title} message={message}></CustomError>
+      </>
+    )
+  } else if (!hasError && !isLoading) {
+    return (
+      <Paper sx={{ p: 1, margin: '2%' }}>
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
+            <ProfileInfo prospect={prospect}/>
+          </Grid>
+          <Grid item>
+            <Divider sx={{ height: '100%' }} orientation="vertical" />
+          </Grid>
+          <Grid item xs>
+            <Typography gutterBottom variant="h6">Notes</Typography>
+            <Divider sx={{ width: 'auto' }} orientation="horizontal" />
+          </Grid>
         </Grid>
-        <Grid item>
-          <Divider sx={{ height: '100%' }} orientation="vertical" />
-        </Grid>
-        <Grid item xs>
-          <Typography gutterBottom variant="h6">Notes</Typography>
-          <Divider sx={{ width: 'auto' }} orientation="horizontal" />
-        </Grid>
-      </Grid>
-    </Paper>
-  )
+      </Paper>
+    )
+  } else {
+    return (
+      <Box sx={{display: 'flex', justifyContent: "center"}}>
+        <Box sx={{fontSize: "2rem", marginTop: "2rem"}}>Page is Loading...</Box>
+      </Box>
+    )
+  }
 }
 
 export default ProspectProfile
