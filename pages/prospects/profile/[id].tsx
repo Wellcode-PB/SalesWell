6d994db729d585767e6e2974e357e8aa05683ff9
Ciprian Dumitriu <dumitriuciprian@gmail.com>
2023-05-11 15:08:39 +0300

    Fixed prospects user page now displays right data

--- pages/prospects/profile/[id].tsx
+++ pages/prospects/profile/[id].tsx
@@ -1,46 +1,85 @@
-import { useEffect, useState } from 'react'
-import { Divider, Grid, Paper, Typography } from '@mui/material'
-import ProfileInfo from '../../../components/prospects/ProfileInfo';
+import { useEffect, useState } from "react";
+import {
+  Box,
+  Container,
+  Divider,
+  Grid,
+  Grow,
+  Paper,
+  Typography,
+} from "@mui/material";
+import ProfileInfo from "../../../components/prospects/ProfileInfo";
+import CustomError from "../../../components/CustomError";
 
 // use this function to get the 'id' parameter from the url
 export async function getServerSideProps({ params }) {
-  return { props: { id: String(params?.id) } }
+  return { props: { id: String(params?.id) } };
 }
 
 function ProspectProfile(props) {
   const [prospect, setProspect] = useState({
-    name: "--", mail: "--", phone: "--", fb: "--"
-  })
+    name: "--",
+    mail: "--",
+    phone: "--",
+    fb: "--",
+  });
+  const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const title = "User not found";
+  const message = "The user you are searching for does not exist.";
 
-  useEffect(() => { 
-    fetch('/api/prospect/get-by-key?key=id&value=' + props.id, {
-      method: 'GET'
+  useEffect(() => {
+    fetch("/api/prospect/get-by-key?key=id&value=" + props.id, {
+      method: "GET",
     })
-    .then(response => response.json())
-    .then((response) => {
-      // the response will be received as an array,
-      // but we only need the first object
-      setProspect(response[0])
-    })
-  }, [])
-
+      .then((response) => response.json())
+      .then((response) => {
+        // the response will be received as an array,
+        // but we only need the first object
+        if (!response[0]) {
+          setHasError(true);
+          setIsLoading(false);
+        } else {
+          setProspect(response[0]);
+          setIsLoading(false);
+        }
+      });
+  }, []);
 
   return (
-    <Paper sx={{ p: 1, margin: '2%' }}>
-      <Grid container spacing={2}>
-        <Grid item xs={4}>
-          <ProfileInfo prospect={prospect}/>
-        </Grid>
-        <Grid item>
-          <Divider sx={{ height: '100%' }} orientation="vertical" />
-        </Grid>
-        <Grid item xs>
-          <Typography gutterBottom variant="h6">Notes</Typography>
-          <Divider sx={{ width: 'auto' }} orientation="horizontal" />
-        </Grid>
-      </Grid>
-    </Paper>
-  )
+    <>
+      {!isLoading ? (
+        <Paper sx={{ p: 1, margin: "2%" }}>
+          {!hasError ? (
+            <Grid container spacing={2}>
+              <Grid item xs={4}>
+                <ProfileInfo prospect={prospect} />
+              </Grid>
+              <Grid item>
+                <Divider sx={{ height: "100%" }} orientation="vertical" />
+              </Grid>
+              <Grid item xs>
+                <Typography gutterBottom variant="h6">
+                  Notes
+                </Typography>
+                <Divider sx={{ width: "auto" }} orientation="horizontal" />
+              </Grid>
+            </Grid>
+          ) : (
+            <>
+              <CustomError title={title} message={message}></CustomError>
+            </>
+          )}
+        </Paper>
+      ) : (
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <Box sx={{ fontSize: "2rem", marginTop: "2rem" }}>
+            Page is Loading...
+          </Box>
+        </Box>
+      )}
+    </>
+  );
 }
 
-export default ProspectProfile
+export default ProspectProfile;
