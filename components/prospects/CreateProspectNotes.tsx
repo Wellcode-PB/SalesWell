bbf0d00b8de169978f1d3f31742b86e5fb212f90
GabrielMaga1 <maga.gabriel@yahoo.com>
2023-05-17 23:32:26 +0300

    prospect-notes-button

---
+++ components/prospects/CreateProspectNotes.tsx
@@ -0,0 +1,31 @@
+import { Box, Button, TextareaAutosize } from "@mui/material";
+import React, { useState } from "react";
+
+
+function Modal({ prospectId, setCloseModal }){
+    const [note, setNote] = useState("");
+
+    const addNote= (e)=> {
+        fetch('/api/prospect/create-note', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({ prospectId, note })
+          })
+        setNote("");
+    }
+    return (
+        <Box className="modalBackground">
+            <Box className="modalContainer">
+                <TextareaAutosize
+                    minRows={5}
+                    style={{ width:'693px' }}
+                    value={note}
+                    onChange={(e)=> setNote(e.target.value)}/>
+                <br/>
+                <Button onClick={addNote} variant="outlined" style={{ marginRight: '0.5rem' }}>submit</Button>
+                <Button onClick={()=> {setCloseModal(false)}} variant="outlined">cancel</Button>
+            </Box>
+        </Box>
+    )
+}
+export default Modal;
